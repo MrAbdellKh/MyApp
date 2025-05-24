137e6b65b887277888611e3f07d2244fec2d46e3
abdelaziz <abdelazizkorchi03@gmail.com>
2025-05-24 08:28:20 +0200

    First commit

---
+++ src/index.php
@@ -0,0 +1,10 @@
+<?php
+// 👋 Affiche un message de bienvenue
+echo "<h1>Bonjour Amigooos</h1>";
+echo "<p>Bienvenue dans le monde de DevOps !</p>";
+
+// Affiche la date et l'heure actuelles
+echo "<p>Date et heure actuelles : " . date('d/m/Y H:i:s') . "</p>";
+
+
+?>
