f29daf42935398caa7515f1951d0135d319bf2fa
abdelaziz <150607413+MrAbdellKh@users.noreply.github.com>
2025-05-26 16:45:08 +0200

    Update index.php

--- src/index.php
+++ src/index.php
@@ -4,6 +4,9 @@ echo "<h1>Bonjour Amigooos</h1>";
 echo "<p>Bienvenue dans le monde de DevOps !</p>";
 
 
+
+
+
 // Affiche la date et l'heure actuelles
 echo "<p>Date et heure actuelles : " . date('d/m/Y H:i:s') . "</p>";
 
